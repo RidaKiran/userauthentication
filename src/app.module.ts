0030dec71dc7ac50099ce904769d002034b162bc
RidaKiran <ridakiran987@gmail.com>
2023-06-22 18:07:59 +0500

    bug fixing

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './user/entities/user.entity';
+import { AppController } from './app.controller';
+import { UserController } from './user/user.controller';
+import { AuthController } from './auth/auth.controller';
+import { AppService } from './app.service';
+import { AuthService } from './auth/auth.service';
+import { UserService } from './user/user.service';
+import { UserRepository } from './user/user.repository';
+import { JwtService } from '@nestjs/jwt';
 
 
 @Module({
@@ -16,5 +24,7 @@ import { UserEntity } from './user/entities/user.entity';
       synchronize: true,
     }),
   ],
+  controllers: [AppController, AuthController, UserController],
+  providers: [AppService, AuthService, UserService, UserRepository, JwtService]
 })
 export class AppModule {}
